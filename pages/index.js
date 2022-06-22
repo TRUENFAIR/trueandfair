109fc13e509f1def982566fb0794ec703ae15a8e
Krishna Chaitanya <krishna.chaitanya@joybird.com>
2022-06-22 10:09:06 +0530

    Home links

--- pages/index.js
+++ pages/index.js
@@ -30,7 +30,7 @@ export default function Home() {
                 
               <p className="prose pt-8">Dr. Deepak Joshi, who&apos;s a renowned Radiologist and Pulmonologist with avid interest in Herbal medicine, had established Madhur Pharma & Research Laboratories in 1986 along with a dedicated team of doctors to revive the Ayurvedic traditions and create high-quality health supplements and personal care products.</p>
 
-              <button className="mt-8 bg-yellow text-black font-bold text-xs hover:text-zinc-700 hover:bg-yellow-800 p-3 rounded-sm transition-all">READ MORE</button>
+              <Link href="/about"><button className="mt-8 bg-yellow text-black font-bold text-xs hover:text-zinc-700 hover:bg-yellow-800 p-3 rounded-sm transition-all">READ MORE</button></Link>
             </div>
             <div className="w-auto h-48 md:h-auto relative">
               <Image className="absolute inset-0 object-cover" layout="fill" alt="" src="/thecompanyhome.jpg"/>
@@ -51,21 +51,30 @@ export default function Home() {
           </div>
         </section>
 
+        {/* <Link href="/services#turnkey-projects">
+<Link href="/services#product-development">
+<Link href="/services#contract-manufacturing"> */}
+
         <section className="md:pt-24 md:pb-32 text-zinc-90">
           <div className="md:container p-4 mx-auto">
             <h1 className="text-4xl tracking-wide font-light text-green-900">WHAT WE OFFER</h1>
             <div className="grid grid-cols-1 md:grid-cols-3 md:gap-12 gap-4 pt-4 md:pt-8">
               
+            <Link href="/services#turnkey-projects">
               <div className="w-auto relative">
                 <div className="relative w-full h-96 flex justify-start items-end shadow-sm hover:scale-[1.02] transition-all cursor-pointer">
                   <Image layout="fill" alt="" src="/turnkeyprojects.jpeg" className="absolute inset-0 object-cover"/>
                   <div className="w-full h-1/2 absolute bottom-0 left-0 right-0 from-transparent bg-gradient-to-b to-black opacity-60"></div>
-                  <div className="flex items-center justify-between z-50 w-full p-4">
-                    <h1 className="text-4xl tracking-wide drop-shadow-md font-semibold  text-white z-10 w-full">Turnkey <br /> Projects</h1>
-                    {rightIcon}
-                  </div>
+                  
+                    <div className="flex items-center justify-between z-50 w-full p-4">
+                      <h1 className="text-4xl tracking-wide drop-shadow-md font-semibold  text-white z-10 w-full">Turnkey <br /> Projects</h1>
+                      {rightIcon}
+                    </div>
                 </div>
-              </div>
+                </div>
+              </Link>
+              
+              <Link href="/services#product-development">
               <div className="w-auto relative">
                 <div className="relative w-full h-96 flex justify-start items-end shadow-sm hover:scale-[1.02] transition-all cursor-pointer">
                   <Image layout="fill" alt="" src="/productdevelopment.jpeg" className="h-96 w-full object-cover absolute inset-0"/>
@@ -76,7 +85,10 @@ export default function Home() {
                     {rightIcon}
                   </div>
                 </div>
-              </div>
+                </div>
+              </Link>
+              
+              <Link href="/services#contract-manufacturing">
               <div className="w-auto relative">
                 <div className="relative w-full h-96 flex justify-start items-end shadow-sm hover:scale-[1.02] transition-all cursor-pointer">
                   <Image layout="fill" alt="" src="/contractmanufacturing.jpeg" className="h-96 w-full object-cover absolute inset-0"/>
@@ -87,7 +99,8 @@ export default function Home() {
                     {rightIcon}
                   </div>
                 </div>
-              </div>
+                </div>
+                </Link>
               </div>
             </div>
         </section>
