136ede3de157a77757fa4f23e8a8f6d907ea0c6e
Krishna Chaitanya <krishna.chaitanya@joybird.com>
2022-07-08 01:53:13 +0530

    Email trigger

--- pages/contact.js
+++ pages/contact.js
@@ -1,7 +1,47 @@
 import Head from "next/head";
 import Image from "next/image";
+import React, { useState } from 'react'
 
 export default function Contact() {
+
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [phone, setPhone] = useState("")
+  const [interest, setInterest] = useState("")
+  const [info, setInfo] = useState("")
+  const [message, setMessage] = useState(false)
+
+  const sendMessage = async (e) => {
+    e.preventDefault()
+
+    if (!!name || !!email || !phone || !info) {
+      return;
+    }
+
+    const response = await fetch( "https://prejyihun6edtdi4tyfymegotm0sbtxi.lambda-url.ap-south-1.on.aws/", {
+      method: 'POST',
+      headers: {},
+      body: JSON.stringify({name, email, phone, info, interest})
+    })
+
+    if (response.status === 200) {
+      setName("")
+      setEmail("")
+      setPhone("")
+      setInterest("")
+      setInfo("")
+      setMessage("Your message is sent. We'll get back to you at the earliest!")
+    } else {
+      console.log(JSON.stringify(response))
+    }
+  }
+
+  const changeName = (e) => { setName(e.target.value) }
+  const changeEmail = (e) => { setEmail(e.target.value) }
+  const changePhone = (e) => { setPhone(e.target.value) }
+  const changeInfo = (e) => { setInfo(e.target.value) }
+  
+
   return (
     <>
       <section className="pt-12 pb-12 text-zinc-900 bg-zinc-50">
@@ -24,22 +64,23 @@ export default function Contact() {
                 </div>
 
               </div>
-              <div className="md:w-3/5 w-full">
+            <div className="md:w-3/5 w-full">
+              
                 <div className="md:pl-12 md:pt-0 pt-12">
                   <h2 className="text-2xl font-light">Write to us</h2>
-                  <div className="mt-8">
+                  <form className="mt-8" onSubmit={sendMessage}>
                     <div className="grid grid-cols-2 gap-6">
                       <label className="block">
                         <span className="text-zinc-500">Name</span>
-                        <input type="text" className="mt-1 block w-full rounded-sm border-zinc-200 focus:border-green-700 focus:ring focus:ring-green-100 focus:ring-opacity-50" placeholder=""/>
+                        <input type="text" className="mt-1 block w-full rounded-sm border-zinc-200 focus:border-green-700 focus:ring focus:ring-green-100 focus:ring-opacity-50" placeholder="" value={name} onChange={changeName}/>
                       </label>
                       <label className="block">
                         <span className="text-zinc-500">Email</span>
-                        <input type="email" className="mt-1 block w-full rounded-sm border-zinc-200 focus:border-green-700 focus:ring focus:ring-green-100 focus:ring-opacity-50" placeholder=""/>
+                        <input type="email" className="mt-1 block w-full rounded-sm border-zinc-200 focus:border-green-700 focus:ring focus:ring-green-100 focus:ring-opacity-50" placeholder="" value={email} onChange={changeEmail}/>
                       </label>
                       <label className="block">
                         <span className="text-zinc-500">Phone</span>
-                        <input type="email" className="mt-1 block w-full rounded-sm border-zinc-200 focus:border-green-700 focus:ring focus:ring-green-100 focus:ring-opacity-50" placeholder=""/>
+                        <input type="tel" className="mt-1 block w-full rounded-sm border-zinc-200 focus:border-green-700 focus:ring focus:ring-green-100 focus:ring-opacity-50" placeholder="" value={phone} onChange={changePhone}/>
                       </label>
                       
                       <label className="block">
@@ -54,12 +95,16 @@ export default function Contact() {
                     </div>
                     <label className="block mt-6">
                       <span className="text-zinc-500">Additional details</span>
-                      <textarea className="mt-1 block w-full rounded-sm border-zinc-200 focus:border-green-700 focus:ring focus:ring-green-100 focus:ring-opacity-50" rows="3"></textarea>
+                    <textarea className="mt-1 block w-full rounded-sm border-zinc-200 focus:border-green-700 focus:ring focus:ring-green-100 focus:ring-opacity-50" rows="3" value={info} onChange={changeInfo}></textarea>
                     </label>
                     <div className="w-full flex justify-center items-center">
+                    {message ? (
+                      <p className="text-green text-md font-semibold pt-6 ">{message}</p>
+                    ): (
                       <button className="w-32 mt-6 bg-yellow text-black font-bold text-xs hover:text-zinc-700 hover:bg-yellow-800 p-3 rounded-sm transition-all">SEND</button>
+                      )}
                     </div> 
-                  </div>
+                  </form>
                 </div>
               </div>
             </div>
@@ -68,7 +113,7 @@ export default function Contact() {
       </section>
 
       <section className="w-full">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3887.190648063656!2d77.51135731509562!3d13.023528217259742!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae3cf61aaaaaab%3A0xa5490ec7bf74d8f6!2sMadhur%20Pharma%20%26%20Research%20Laboratories%20Private%20Limited!5e0!3m2!1sen!2sin!4v1656776970215!5m2!1sen!2sin" width="100%" height="600" allowFullScreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3887.190648063656!2d77.51135731509562!3d13.023528217259742!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae3cf61aaaaaab%3A0xa5490ec7bf74d8f6!2sMadhur%20Pharma%20%26%20Research%20Laboratories%20Private%20Limited!5e0!3m2!1sen!2sin!4v1656776970215!5m2!1sen!2sin" width="100%" height="600" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
       </section>
     </>
   );
--- pages/index.js
+++ pages/index.js
@@ -110,7 +110,7 @@ export default function Home() {
           </div>
         </section>
 
-        <Clients/>
+        {/* <Clients/> */}
 
         <Testimonials/>
 
