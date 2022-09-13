83b5917ca8eb84f8b3dfc810bf2297e86cec28a1
JEREMIA-SUNNY <you@example.com>
2022-09-13 13:03:40 +0530

    XDSF

--- pages/_app.js
+++ pages/_app.js
@@ -37,7 +37,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Head>
-        <title>{`True & Fair`}</title>
+        <title>{`True And Fair`}</title>
         <meta  name="description" content={`Official website of True&Fair`} />
         <link rel="icon" href="/homeicon/tnf.png" />
       </Head>
